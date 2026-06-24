942b4dc7c7fa4621b3d8a03cb3b2dbb2f42fb72e
web-aadii <adityarajchaudhari11@gmail.com>
2026-06-24 23:30:35 +0530

    take input from usser and greeting output

---
+++ basics/hello.cpp
@@ -0,0 +1,10 @@
+// print simple hello message using c++
+
+#include<iostream>
+using namespace std;
+int main(){
+    cout<<"Hello bhaillog";
+
+    return 0;
+
+}
---
+++ basics/io_name.cpp
@@ -0,0 +1,13 @@
+//Take name as input and print:- "Hello,Aditya"
+//approch :- 
+
+#include<iostream>
+using namespace std;
+int main(){
+    
+    cout<<"Enter your name: ";
+    string name;
+    cin>> name;
+    cout<<"hello "<< name;
+    return 0;
+}
